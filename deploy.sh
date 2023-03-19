6dbb3653877ca426d4c9b74b883ae821351a3a0f
Ravish <211128@iiitt.ac.in>
2023-03-19 20:13:25 +0530

    removing extra files

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 echo "# qrcode-frontend-mentor" >> README.md
-git init
-git add README.md
 git add .
-git commit -m "first commit on qrcode-frontend-mentor"
-git branch -M main
-git remote add origin https://github.com/Ravish108Coder/qrcode-frontend-mentor.git
+git commit -m "removing extra files"
 git push -u origin main
