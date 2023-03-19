32a27c96f774150ea27fd307de91ac733a5fad09
Ravish <211128@iiitt.ac.in>
2023-03-19 20:11:23 +0530

    first commit on qrcode-frontend-mentor

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+echo "# qrcode-frontend-mentor" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit on qrcode-frontend-mentor"
+git branch -M main
+git remote add origin https://github.com/Ravish108Coder/qrcode-frontend-mentor.git
+git push -u origin main
